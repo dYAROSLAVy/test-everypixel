f66b0a6f22d15b66d36da752eebf54ba6373934d
dYAROSLAVy <yadsp@yandex.ru>
2025-10-31 11:54:32 +0300

    feat: router + protected routes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,31 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { LoginPage } from "../pages/login";
+import { HomePage } from "../pages/home";
+import { ProtectedRoute, PublicRoute } from "../shared/lib/protected-route";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
